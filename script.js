0899d0ab337a7a8436a344fcfa70f4f3731ba58b
imdvs07 <60706340+imdvs07@users.noreply.github.com>
2020-12-05 02:29:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-$(".slideshow-container > div:gt(0)").hide();
+/*$(".slideshow-container > div:gt(0)").hide();
 
 setInterval(function() {
   $('.slideshow-container > div:first')
@@ -8,3 +8,24 @@ setInterval(function() {
     .end()
     .appendTo('.slideshow-container');
 }, 4500);
+*/
+
+var slideshows = document.querySelectorAll('.slideshow-container')
+slideshows.forEach(initSlideShow);
+
+function initSlideShow(slideshow){
+    var slides = document.querySelectorAll('.cr');
+
+    var index = 0, time = 5000;
+    slides[index].classList.add('active');
+
+    setInterval(() => {
+        slides[index].classList.remove('active');
+
+        index++;
+
+        if(index === slides.length) index = 0;
+
+        slides[index].classList.add('active');
+    }, time);
+}
